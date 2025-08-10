9dcfba1b2d4bdea241cf537c4ddda90b32194268
Jacob Frøkjær Larsen <jacob-f-larsen@hotmail.com>
2025-08-10 23:02:20 +0200

    Allowing orphans
    Foreign keys are disabled while it truncates

--- modules/mod-outland/data/sql/db-world/002_outland_clean_world_spawns.sql
+++ modules/mod-outland/data/sql/db-world/002_outland_clean_world_spawns.sql
@@ -1,6 +1,9 @@
--- [Outland] Issue #3: Clean World Spawns
+(v2 - Foreign Key Fix)
 -- Description: Removes all default creature and gameobject spawns from the world,
 -- and severs all links between entities and quests to create a blank slate.
+
+-- Temporarily disable foreign key checks to allow truncation of linked tables.
+SET FOREIGN_KEY_CHECKS=0;
 
 -- Empty all creature spawn data. This will remove every NPC from the world.
 TRUNCATE TABLE `creature`;
@@ -22,3 +25,9 @@ TRUNCATE TABLE `gameobject_questender`;
 
 -- This table holds waypoints for creatures. Since there are no creatures, we clear this.
 TRUNCATE TABLE `waypoint_data`;
+
+-- Also truncate the sparring table that caused the error.
+TRUNCATE TABLE `creature_sparring`;
+
+-- Re-enable foreign key checks to restore database integrity.
+SET FOREIGN_KEY_CHECKS=1;
