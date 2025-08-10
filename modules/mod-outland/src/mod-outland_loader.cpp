08c1b71ac2be0b4f85108f1683bff5c0bc56256d
Jacob Frøkjær Larsen <jacob-f-larsen@hotmail.com>
2025-08-10 22:44:12 +0200

    understanding structure

---
+++ modules/mod-outland/src/mod-outland_loader.cpp
@@ -0,0 +1,16 @@
+
+// From SC
+void Addmod_outlandScripts();
+
+// Add all scripts from the mod-outland module
+void Addmod_outlandScripts()
+{
+    // This is where we will register our custom C++ scripts later.
+}
+
+// The core needs this function to exist, even if it does nothing.
+// We provide a blank definition here to satisfy the C++ linker.
+void AddCustomScripts()
+{
+    // Leave this function empty.
+}
